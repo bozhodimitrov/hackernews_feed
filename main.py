3885918738f1ceabba09328b5d1c52863f22ea3e
int3l <int3l@users.noreply.github.com>
2020-08-19 23:47:44 +0300

    Add website fallback fetching method
    because the Firebase API may consistently return `null` for random story items

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
 import asyncio
 import aiohttp
+import json
+import re
+import time
 from contextlib import suppress
+from functools import wraps
 from datetime import datetime
 from aiosseclient import aiosseclient
 from colorama import Fore, Back, Style
@@ -9,32 +12,70 @@ from colorama import Fore, Back, Style
 
 FETCH_ATTEMPTS = 3
 FETCH_RETRY_DELAY = 5
+
 BASE_URL = 'https://hacker-news.firebaseio.com/v0'
 STORIES_URL = f'{BASE_URL}/newstories.json'
 ITEM_URL = f'{BASE_URL}/item'
+WEB_ITEM_URL = f'https://news.ycombinator.com/item?id='
+INVALID_START = f'\n\n{Style.NORMAL}{Fore.RED}'
+INVALID_END = f'{Style.RESET_ALL}\n\n'
+ITEM_PATTERN = re.compile(
+    r'"title"><a href="(?P<url>.*?)".*?>(?P<title>.*?)</a>'
+)
+
 HEADERS = { 'Accept': 'application/json' }
 SSE_TIMEOUT = aiohttp.ClientTimeout(
     total=None, connect=None, sock_connect=None, sock_read=None
 )
-INVALID_START = f'\n\n{Style.NORMAL}{Fore.RED}'
-INVALID_END = f'{Style.RESET_ALL}\n\n'
 
 
-async def fetch_story(story_id):
+def retry(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        for _ in range(FETCH_ATTEMPTS):
+            if result := await func(*args, **kwargs):
+                break
+            else:
+                await asyncio.sleep(FETCH_RETRY_DELAY)
+        return result
+
+    return wrapper
+
+
+@retry
+async def api_fetch(story_id):
     url = f'{ITEM_URL}/{story_id}.json'
     async with aiohttp.ClientSession() as session:
-        for _ in range(FETCH_ATTEMPTS):
-            with suppress(aiohttp.client_exceptions.ClientConnectorError):
-                async with session.get(url, headers=HEADERS) as resp:
-                        if resp.status != 200:
-                            print(f'{resp.status} error')
-                            return
+        with suppress(aiohttp.client_exceptions.ClientConnectorError):
+            async with session.get(url, headers=HEADERS) as resp:
+                if resp.status == 200:
+                    return await resp.json()
 
-                        result = await resp.json()
-                        if result != None:
-                            return result
-                
-            await asyncio.sleep(FETCH_RETRY_DELAY)
+
+@retry
+async def web_fetch(story_id, timestamp):
+    url = f'{WEB_ITEM_URL}{story_id}'
+    async with aiohttp.ClientSession() as session:
+        with suppress(aiohttp.client_exceptions.ClientConnectorError):
+            async with session.get(url, headers=HEADERS) as resp:
+                if resp.status == 200:
+                    html = await resp.text()
+                    if match := ITEM_PATTERN.search(html):
+                        return {
+                            'id': story_id,
+                            'url': match.group('url'),
+                            'title': match.group('title'),
+                            'time': timestamp,
+                        }
+
+
+async def fetch(story_id, timestamp):
+    if story := await api_fetch(story_id):
+        return story
+    elif story := await web_fetch(story_id, timestamp):
+        return story
+    else:
+        print(f'{INVALID_START}{story_id}{INVALID_END}')
 
 
 async def announce(story):
@@ -64,10 +105,7 @@ async def hackernews_feed():
                     continue
 
                 max_story_id = story_id
-                story = await fetch_story(story_id)
-                if story == None:
-                    print(f'{INVALID_START}{story_id}{INVALID_END}')
-                else:
+                if story := await fetch(story_id, time.time()):
                     yield story
             else:
                 announce.enable = True
