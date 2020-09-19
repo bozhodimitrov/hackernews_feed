3390bba603e2bf0ca72517d4a5a42404a5760bf4
int3l <int3l@users.noreply.github.com>
2020-09-19 23:54:52 +0300

    Report proper exit code in case of interrupt

--- main.py
+++ main.py
@@ -140,5 +140,7 @@ async def main():
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    with suppress(KeyboardInterrupt):
+    try:
         loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        raise SystemExit(130)
